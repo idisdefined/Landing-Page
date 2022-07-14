85a445ae21eea9970817ab6dc84f55edd1c5e893
Azamat <78691610+idisdefined@users.noreply.github.com>
2022-07-15 01:50:51 +0300

    Updated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Menu slide
+
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.header-title');
 
@@ -6,4 +8,22 @@ if (menuBtn) {
         menuBtn.classList.toggle('active');
         menu.classList.toggle('active');
     })
+}
+
+
+// Scroll
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
 }
